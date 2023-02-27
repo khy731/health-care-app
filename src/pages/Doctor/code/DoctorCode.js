08d259bf030fb63123380bb5edc6bda752bf5afe
KangHyeYoon <khy00731@gmail.com>
2023-02-27 13:21:14 +0900

    의사/환자 코드발급 로직 수정

--- src/pages/Doctor/code/DoctorCode.js
+++ src/pages/Doctor/code/DoctorCode.js
@@ -1,62 +1,64 @@
-
 import React, { useState, useEffect} from 'react';
 
 import Card from '../../../components/UI/Card';
 import Button from '../../../components/UI/Button';
 
 const DoctorCode = (props) => {
-    const [id, setId] = useState("");
-    const [code, setCode] = useState("");
+  const [id, setId] = useState("");
+  const [code, setCode] = useState("");
 
-    useEffect(() => {
-        if(sessionStorage.getItem('doctor_id') === null){
-          console.log('회원 정보가 없습니다. 로그인하세요.');
-        } else {
-          setId(sessionStorage.getItem('doctor_id'));
-        }
-      });
+  useEffect(() => {
+    if (sessionStorage.getItem('doctor_id') === null) {
+      console.log('회원 정보가 없습니다. 로그인하세요.');
+    } else {
+      setId(sessionStorage.getItem('doctor_id'));
+    }
+  }, []);
 
-  const DoctorCodeHandler = (event) => {
-    event.preventDefault();
-
+  const handleCodeGeneration = () => {
     const newCode = Math.random().toString(16).slice(2, 8);
     setCode(newCode);
-    console.log(code);
+  };
 
-    fetch(`http://localhost:8080/doctor/${id}/code`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        data : {
+  const handleDoctorCodeSubmit = async (event) => {
+    event.preventDefault();
+    handleCodeGeneration();
+
+    try {
+      const response = await fetch(`http://localhost:8080/doctor/${id}/code`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          data : {
             code: code
-        }
-      }),
-    })
-  .then((response) => response.json())
-  .then(res => {
-    if (res.result === 'OK') {
+          }
+        }),
+      });
+      const data = await response.json();
+      if (data.result === 'OK') {
         alert("매칭이 완료되었습니다");
-    }
-    if (res.result === "Fail") {
+      } else if (data.result === "Fail") {
         alert("서버 오류로 매칭이 실패했습니다. 다시 시도해주세요.");
-    }
-    else {
+      } else {
         alert("잘못된 시도입니다.");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("서버 연결 중 오류가 발생했습니다.");
     }
-  });
-}
+  }
 
   return (
-      <Card>
-        <h2>연결 코드 발급</h2>
-        <form onSubmit={DoctorCodeHandler}>
-            <label>코드를 발급하면 환자와 연결이 가능해집니다.</label>
-          <input id="username" type="text" value={code}/>
-          <Button type="submit">발급받기</Button>
-        </form>
-      </Card>
+    <Card>
+      <h2>연결 코드 발급</h2>
+      <form onSubmit={handleDoctorCodeSubmit}>
+        <label>코드를 발급하면 환자와 연결이 가능해집니다.</label>
+        <input id="username" type="text" value={code} readOnly />
+        <Button type="submit">발급받기</Button>
+      </form>
+    </Card>
   );
 };
 
--- src/pages/Patient/code/PatientCode.js
+++ src/pages/Patient/code/PatientCode.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from 'react';
 
 import Card from '../../../components/UI/Card';
@@ -15,50 +14,49 @@ const PatientCode = (props) => {
         } else {
           setId(sessionStorage.getItem('patient_id'));
         }
-      });
+    }, [id]);
 
-  const onChangeHandler = e => {
-    setCode(e.target.value);
-  }
-
-  const PatientCodeHandler = (event) => {
-    event.preventDefault();
-
-    fetch(`http://localhost:8080/patient/${id}/code`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        data : {
-            code: code
-        }
-      }),
-    })
-  .then((response) => response.json())
-  .then(res => {
-    if (res.result === 'OK') {
-        alert("매칭이 완료되었습니다");
+    const onChangeHandler = e => {
+        setCode(e.target.value);
     }
-    if (res.result === "Fail") {
-        alert("서버 오류로 매칭이 실패했습니다. 다시 시도해주세요.");
-    }
-    else {
-        alert("잘못된 시도입니다.");
-    }
-  });
-}
 
-  return (
-      <Card>
-        <h2>연결 코드 입력</h2>
-        <form onSubmit={PatientCodeHandler}>
-            <label>코드를 입력하면 담당의와 연결됩니다.</label>
-          <input id="username" type="text" value={code} onChange={onChangeHandler} placeholder="코드를 입력하세요."/>
-          <Button type="submit">입력하기</Button>
-        </form>
-      </Card>
-  );
+    const PatientCodeHandler = (event) => {
+        event.preventDefault();
+
+        fetch(`http://localhost:8080/patient/${id}/code`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                data : {
+                    code: code
+                }
+            }),
+        })
+        .then((response) => response.json())
+        .then(res => {
+            if (res.result === 'OK') {
+                alert("매칭이 완료되었습니다");
+            } else if (res.result !== "Fail") {
+                alert("서버 오류로 매칭이 실패했습니다. 다시 시도해주세요.");
+            }
+        })
+        .catch((error) => {
+            alert("서버와의 연결이 끊겼습니다. 다시 시도해주세요.");
+        });
+    }
+
+    return (
+        <Card>
+            <h2>연결 코드 입력</h2>
+            <form onSubmit={PatientCodeHandler}>
+                <label>코드를 입력하면 담당의와 연결됩니다.</label>
+                <input id="username" type="text" value={code} onChange={onChangeHandler} placeholder="코드를 입력하세요."/>
+                <Button type="submit">입력하기</Button>
+            </form>
+        </Card>
+    );
 };
 
 export default PatientCode;
