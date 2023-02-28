a7f93d2fc639729112d941fcb7760b8689d47a0e
KangHyeYoon <khy00731@gmail.com>
2023-02-28 13:55:44 +0900

    진단하기 버튼 추가

--- src/pages/Doctor/Treat/DoctorTreat.js
+++ src/pages/Doctor/Treat/DoctorTreat.js
@@ -2,11 +2,17 @@ import { useState } from "react";
 import Diagnosis from "../../../components/Doctor/Treat/Diagnosis";
 import TreatList from "../../../components/Doctor/Treat/TreatList";
 import TreatPatientList from "../../../components/Doctor/Treat/TreatPatientList";
+import Button from "../../../components/UI/Button";
 
 const DoctorTreat = () => {
   const [selectedList, setSelectedList] = useState("patient4");
   const [selectedTreat, setSelectedTreat] = useState("");
   const [selectedDiaNum, setSelectedDiaNum] = useState("");
+  const [isClicked, setIsClicked] = useState(false);
+
+  const onClickHandler = () => {
+    setIsClicked(true);
+  };
 
   return (
     <>
@@ -18,10 +24,13 @@ const DoctorTreat = () => {
         selectedList={selectedList}
         setSelectedDiaNum={setSelectedDiaNum}
       />
-      <Diagnosis
-        selectedTreat={selectedTreat}
-        selectedDiaNum={selectedDiaNum}
-      />
+      {!isClicked && <Button onClick={onClickHandler}>처방하기</Button>}
+      {isClicked && (
+        <Diagnosis
+          selectedTreat={selectedTreat}
+          selectedDiaNum={selectedDiaNum}
+        />
+      )}
     </>
   );
 };
