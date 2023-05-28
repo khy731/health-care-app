0e543ad010547ad4a755b83b14a30ef9b9b55fe6
KangHyeYoon <khy00731@gmail.com>
2023-05-28 22:05:26 +0900

    충돌 해결

--- src/components/Patient/Sensor/HealthBox.js
+++ src/components/Patient/Sensor/HealthBox.js
@@ -2,27 +2,13 @@ import React from "react";
 
 import classes from "./HealthBox.module.css";
 const HealthBox = ({ title, value, isNormal }) => {
-
   const boxColor = isNormal ? "#7CFC00" : "#228B22";
 
   return (
-     <div className={classes.healthbox}>
+    <div className={classes.healthbox}>
       <div className={classes.name}>{title}</div>
-      <div className={classes.value} style={{backgroundColor: boxColor}}>
-        현재 수치: {value}
-      </div>
-      <div
-        style={{
-          width: "100px",
-          height: "100px",
-          backgroundColor: boxColor,
-          color: "#fff",
-          fontWeight: "bold",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+      <div>현재 수치: {value}</div>
+      <div className={classes.value} style={{ backgroundColor: boxColor }}>
         {isNormal ? "정상" : "비정상"}
       </div>
     </div>
