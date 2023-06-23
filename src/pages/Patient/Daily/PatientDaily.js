4b86b2cebbe3895ce648be8265d5e0260009e1ba
KangHyeYoon <khy00731@gmail.com>
2023-06-24 00:42:04 +0900

    간단한 수정

--- src/pages/Patient/Daily/PatientDaily.js
+++ src/pages/Patient/Daily/PatientDaily.js
@@ -3,6 +3,9 @@ import PatientDailyForm from "../../../components/Patient/Daily/PatientDailyForm
 const PatientDaily = () => {
 return(
     <div>
+        <div>
+            <h2>일기 작성하기</h2>
+        </div>
         <PatientDailyForm />
     </div>  
 );
